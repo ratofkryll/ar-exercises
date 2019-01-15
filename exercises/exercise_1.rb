ae8852d7742e7cecd0568c152d5a233f8c63c8a1
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-15 20:12:18 +0000

    completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+stores = Store.create([
+                       {
+                        name: 'Burnaby',
+                        annual_revenue: 300_000,
+                        mens_apparel: true,
+                        womens_apparel: true
+                       },
+                      {
+                        name: 'Richmond',
+                        annual_revenue: 1_260_000,
+                        mens_apparel: false,
+                        womens_apparel: true
+                      },
+                      {
+                        name: 'Gastown',
+                        annual_revenue: 190_000,
+                        mens_apparel: true,
+                        womens_apparel: false
+                      }
+])
+
+puts "Stores Count: #{stores.count}"
